456ffb14083034a03e88574cbabdac8c18be4f28
Mickael Rocha <mickael.n.xd@gmail.com>
2020-07-14 23:46:15 -0300

    Projeto Bin2Dec

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function validInput(){
+  const input = document.querySelector('input').value;
+  const result = document.getElementById('result');
+
+  for(var i = 0; i < input.length; i++){
+    if(input[i] != 0 && input[i] != 1){
+      result.innerHTML = `O número ${input[i]} está errado, favor inserir 0 ou 1.`;
+      result.style = "color: yellow;"
+      return;
+    }
+  }
+
+  const converted = parseInt(input, 2).toString(10);
+  result.innerHTML = converted;
+
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>Binary: </span>
+        <input type="text" maxLength={8} onBlur={() => validInput()}/>
+      </div>
+      <div>
+        <span>Output: </span>
+        <span id="result"></span>
+      </div>
     </div>
+    // Quando converter, se tiver letra ou algo diff 0/1 vai avisar
   );
 }
 
